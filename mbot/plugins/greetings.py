43af44704221fa2fe83554949e36c57ac49fbbd2
Masterolic <rockingmallu8@gmail.com>
2024-03-31 16:10:31 +0530

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -46,9 +46,6 @@ async def start(client,message):
 
         invite_link = await client.create_chat_invite_link(chat_id=(int(LOG_GROUP) if str(LOG_GROUP).startswith("-100") else LOG_GROUP))
         reply_markup.append([InlineKeyboardButton("LOG Channel", url=invite_link.invite_link)])
-    if message.chat.type != "private" and message.chat.id not in AUTH_CHATS and message.from_user.id not in SUDO_USERS:
-        return await message.reply_text("This Bot Will Not Work In Groups Unless It's Authorized.",
-                    reply_markup=InlineKeyboardMarkup(reply_markup))
     return await message.reply_text(f"Hello {message.from_user.first_name}, I'm a Simple Music Downloader Bot. I Currently Support Download from Youtube.",
                     reply_markup=InlineKeyboardMarkup(reply_markup))
 
