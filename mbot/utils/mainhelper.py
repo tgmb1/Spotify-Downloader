e93b991009ad10611ed268b1129ea1af396bb1a2
Masterolic <rockingmallu8@gmail.com>
2024-08-09 20:21:42 +0530

    Update mainhelper.py

--- mbot/utils/mainhelper.py
+++ mbot/utils/mainhelper.py
@@ -240,7 +240,7 @@ def download_dez(song, download_directory='.'):
             "prefer_ffmpeg": False,
             "geo_bypass": True,
             "nocheckcertificate": True,
-            "proxy": f"socks5://{FIXIE_SOCKS_HOST}",
+            "proxy": FIXIE_SOCKS_HOST,
             "postprocessors": [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'flac', 'preferredquality': '824'}],
              }
              try:
